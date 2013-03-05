eab7fbd19ab0c03be3f55833cfc4af1bd168f2e3
luchux <lucianoguasco@gmail.com>
2013-03-05 22:34:48 +1100

    added attributes to model review

--- arguiew/reviews/models.py
+++ arguiew/reviews/models.py
@@ -1,13 +1,81 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models import ForeignKey
+from django.forms import ModelForm
+from classifier.functions import analyze_text
+import ast
 
-# Create your models here.
+class ListField(models.TextField):
+    __metaclass__ = models.SubfieldBase
+    description = "Stores a python list"
 
-### Review #######
-### Represents the Review that contains the opinion, features, etc. 
-### TODO: all :) 
+    def __init__(self, *args, **kwargs):
+        super(ListField, self).__init__(*args, **kwargs)
+
+    def to_python(self, value):
+        if not value:
+            value = []
+
+        if isinstance(value, list):
+            return value
+
+        return ast.literal_eval(value)
+
+    def get_prep_value(self, value):
+        if value is None:
+            return value
+
+        return unicode(value)
+
+    def value_to_string(self, obj):
+        value = self._get_val_from_obj(obj)
+        return self.get_db_prep_value(value)
+
+class Product(models.Model):
+	#represent client products
+	name = models.CharField('name',max_length=100)
+	description = models.CharField('descripton',max_length=200)
+	
+	def __unicode__(self):
+		return self.name
+
 
 class Review(models.Model):
-	text = models.CharField(blank=True,max_length=200)
+	#represent users reviews about products
+	positive_text = models.CharField('pos',max_length=200)
+	negative_text = models.CharField('neg',max_length=200)
+	positive_feats = ListField(blank=True, null=True,editable=False)
+	negative_feats = ListField(blank=True, null=True,editable=False)
+	undecided_feats = ListField(blank=True, null=True,editable=False)
+	rating = models.IntegerField(blank=True, null=True)
+	product = models.ForeignKey(Product)
+	user = models.ForeignKey(User)
 
-### End Review ###
+	def __unicode__(self):
+		return self.product.name + ' - ' + self.user.username
 
+	def save(self, *args, **kwargs):
+		#This is a patch. This should call the propper API to process the text
+		#and get the features back. 
+		self.positive_feats = analyze_text(self.positive_text)
+		self.negative_feats = analyze_text(self.negative_text)
+
+		super(Review,self).save()
+		#call to the api that process positive, negative and store it in the dbase. 
+
+class ReviewForm(ModelForm):
+	class Meta:
+		model = Review
+		fields = ('positive_text','negative_text','rating','product')
+
+	def __init__(self, user, *args, **kwargs):
+		self.user = user
+		super(ReviewForm, self).__init__(*args, **kwargs)
+	
+	#save is overriden to take into account the user logged in (user passed in the constructor)
+	def save(self, *args, **kwargs):
+		kwargs['commit']=False #with commit false, first create the object then call object.save
+		obj = super(ReviewForm, self).save(*args, **kwargs)
+		if self.user:
+		   obj.user = self.user
+		obj.save()	
